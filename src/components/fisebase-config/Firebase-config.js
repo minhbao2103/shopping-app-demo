fcb9ec8a65aa576dc197d4668acaf6c16bbc35ba
MSI\Admin <trinhminhbao21031996@gmail.com>
2023-01-08 09:38:42 +0700

    test

--- src/components/fisebase-config/Firebase-config.js
+++ src/components/fisebase-config/Firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth"
 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId:  process.env.REACT_APP_FIREBASE_PROJECT_ID ,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET  ,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER  ,
-    appId: process.env.REACT_APP_APP_ID  ,
-    measurementId: process.env.REACT_APP_MEASURE
+    apiKey: "AIzaSyDeWVAaxTDE_Qlf2CAjw7Iw6ciy_Npyjd0",
+    authDomain: "authentication-user-7a7e5.firebaseapp.com",
+    projectId: "authentication-user-7a7e5" ,
+    storageBucket: "authentication-user-7a7e5.appspot.com"  ,
+    messagingSenderId: "446018146874" ,
+    appId:"1:446018146874:web:27f8fe39dac2a3a0755b03" ,
+    measurementId: "G-TVGT5V8B7H"
   };
 
 
